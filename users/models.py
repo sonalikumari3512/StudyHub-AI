27846349899322bdfdd20da5a49d8afb73a3dc75
sonali kumari <sonalikumari3042@gmail.com>
2026-07-04 16:02:13 +0530

    Day 5: Created UserProfile model and database

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+
+    full_name = models.CharField(max_length=100)
+
+    college = models.CharField(max_length=100)
+
+    year = models.IntegerField()
+
+    branch = models.CharField(max_length=100)
+
+    def __str__(self):
+
+        return self.full_name
