2cff06d2e1590bcab43c4f76f748d1a90bd3d748
sonali kumari <sonalikumari3042@gmail.com>
2026-07-05 17:36:04 +0530

    Day 6: Added user registration form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserProfileForm
 
-# Create your views here.
 def home(request):
-    return render(request, 'users/home.html')
+    return render(request, "users/home.html")
+
+
+def register(request):
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/") 
+
+    else:
+        form = UserProfileForm()
+
+    return render(request, "users/register.html", {"form": form})
