446329ca293a28a981f8e1a1b35308600ba7453e
sonali kumari <sonalikumari3042@gmail.com>
2026-07-06 18:11:35 +0530

    Day 7: Added Students dashboard

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path("students/", views.students, name="students"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserProfileForm
+from django.contrib import messages
+from .models import UserProfile
 
 def home(request):
     return render(request, "users/home.html")
@@ -12,9 +14,19 @@ def register(request):
 
         if form.is_valid():
             form.save()
-            return redirect("/") 
+            messages.success(request, "Registration Successful!")
+            return redirect("register")
 
     else:
         form = UserProfileForm()
 
     return render(request, "users/register.html", {"form": form})
+
+def students(request):
+    students = UserProfile.objects.all()
+
+    return render(
+        request,
+        "users/students.html",
+        {"students": students}
+    )
