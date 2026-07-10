d20e4c4ddf8090c0cc1e68dc3da23868c79fc189
sonali kumari <sonalikumari3042@gmail.com>
2026-07-10 18:30:02 +0530

    Day 11: Created Dashboard app and user dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from users.models import UserProfile
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    profile = UserProfile.objects.get(user=request.user)
+
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {
+            "profile": profile
+        }
+    )
