916ea1c9de960a58db744e1252474fb914d53ea3
sonali kumari <sonalikumari3042@gmail.com>
2026-07-11 18:40:56 +0530

    Day 12: Created Room model and registered in Django admin

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Room(models.Model):
+
+    host = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
