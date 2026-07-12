e1991fb15437e4d7cd80ffd1ce9c73f21e88db79
sonali kumari <sonalikumari3042@gmail.com>
2026-07-12 20:39:30 +0530

    Day 13: Added room creation with ModelForm and automatic host assignment

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RoomForm
 
-# Create your views here.
+@login_required
+def createRoom(request):
+
+    form = RoomForm()
+
+    if request.method == "POST":
+
+        form = RoomForm(request.POST)
+
+        if form.is_valid():
+
+            room = form.save(commit=False)
+
+            room.host = request.user
+
+            room.save()
+
+            return redirect("/dashboard/")
+
+    context = {
+        "form": form
+    }
+
+    return render(
+        request,
+        "rooms/create_room.html",
+        context
+    )
--- studyhub/urls.py
+++ studyhub/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')),
     path("dashboard/", include("dashboard.urls")),
+    path("rooms/", include("rooms.urls")),
 ]
